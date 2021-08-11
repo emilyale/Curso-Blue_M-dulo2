c9a2d33e54068e603a59adaedc33eb991ed515e0
Emily <47272648+emilyale@users.noreply.github.com>
2021-08-11 09:13:53 -0300

    Add files via upload

---
+++ Portifolio/script.js
@@ -0,0 +1,10 @@
+let btnEnviar = document.querySelector('#enviar')
+
+// if (nomeOk && emailOk && msgOk) {
+//     btnEnviar.disabled = false
+//  } else { 
+//     btnEnviar.disabled = true
+//  }
+function btn(){ 
+    alert('Mensagem enviada');
+}
